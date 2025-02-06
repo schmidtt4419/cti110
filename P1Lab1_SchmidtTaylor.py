d37129434461ac97857e7ff05d516465edbb9f0e
schmidtt4419 <schmidtt4419@student.faytechcc.edu>
2025-02-06 11:36:24 -0500

    Add files via upload
    1st assignment

---
+++ P1Lab1_SchmidtTaylor.py
@@ -0,0 +1,12 @@
+#Taylor Schmidt
+#02/04/2025
+#P1Lab1
+#How to get input from user and display output
+
+#Get user first and last name
+user_first_name = input("Enter your first name: ")
+user_last_name = input("Enter your last name: ")
+
+#display welcome message using first and last name
+print("Hello,", user_first_name, user_last_name + "!", "Welcome to CTI-110")
+
