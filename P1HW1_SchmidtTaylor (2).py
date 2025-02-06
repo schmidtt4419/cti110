39545604ae753a1885b25247374e5aca54a94aba
schmidtt4419 <schmidtt4419@student.faytechcc.edu>
2025-02-06 11:36:58 -0500

    Add files via upload
    2nd Assignment

---
+++ P1HW1_SchmidtTaylor (2).py
@@ -0,0 +1,44 @@
+﻿#TayorSchmidt
+#02-06-2025
+#P1HW1
+#Write code that collects information from user, processes information collected and display results to user.
+
+#Calculating exponents
+
+#Ask the user for the base value
+base = int(input("Enter an integer as the base value: "))
+
+#Ask the user for the exponent value
+exponent = int(input("Enter an integer as the exponent: "))
+
+#Calculate the result
+result = base ** exponent
+
+#Add blank line
+print("")
+
+#Print the result
+print(base,"Raised to the power of", exponent, "is", result, "!!")
+
+#Add blank line
+print("")
+
+#Addition and Subtraction
+
+#Ask user for starting integer
+num_1= int(input("Enter a starting integer: "))
+
+#Ask user for integer to add
+num_2= int(input("Enter an integer to add: "))
+
+#Ask user for integer to subtarct
+num_3= int(input("Enter an integer to subtract: "))
+
+#Calculate the result
+result= num_1 + num_2 - num_3
+
+#Add blank line
+print("")
+
+#Print the result
+print(num_1, "+", num_2, "-", num_3, "is equal to", result)
