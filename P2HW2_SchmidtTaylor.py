5554b8b15922db044e3bfa309a403c632d9ca67e
schmidtt4419 <schmidtt4419@student.faytechcc.edu>
2025-02-25 11:00:12 -0500

    Add files via upload
    First Submission

---
+++ P2HW2_SchmidtTaylor.py
@@ -0,0 +1,36 @@
+#Taylor Schmidt
+#2/25/25
+#P2HW2
+#For assignment this assignment, you are to design a program that does the following: ask the user to enter grades for Modules 1-6, store grades entered in a list, and display the lowest grade.. highest garde.. sum of grades.. and grade average
+
+#Input grades from the user and put them into this list
+module_grades = []
+
+#Ask the user to input grades for each module (W3 Schools Loop Lists)
+#i represets index...
+for i in range(1, 7): 
+    grade = float(input(f"Enter the grade for Module {i}: "))
+    module_grades.append(grade)
+
+#Calculations to find min, max, sum, and average
+#Find the lowest grade
+lowest_grade = min(module_grades)  
+
+#Find the highest grade
+highest_grade = max(module_grades)
+
+#Find the sum of all grades
+sum_of_grades = sum(module_grades)
+
+#Find the average of all grades
+#len() is a built-in function in Python that returns the number of items in an object.. AKA 6 for this assignment becue there are 6 modules
+
+average_grade = sum_of_grades / len(module_grades)
+
+#Display the results
+print("\n-----------Results------------")
+print(f"Lowest grade: {lowest_grade}")
+print(f"Highest grade: {highest_grade}")
+print(f"Sum of grades: {sum_of_grades}")
+print(f"Average grade: {average_grade:.2f}")
+print("---------------------------------------")
