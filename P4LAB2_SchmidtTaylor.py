b5ff3de17173264556ac41fbf71d2f0b6afffce3
schmidtt4419 <schmidtt4419@student.faytechcc.edu>
2025-03-20 11:37:52 -0400

    Add files via upload

---
+++ P4LAB2_SchmidtTaylor.py
@@ -0,0 +1,22 @@
+#Taylor Schmidt
+#P4LAB2
+#3/20/25
+#Working with nested loops
+
+# Write a variable to control the loops
+run_again = "yes"
+
+# While loop to control the main logic
+while run_again != "no":
+    integer = int(input("Enter an integer: "))
+    if integer >=0: 
+        for i in range (1,13):
+            print(f"{integer} * {i} = {integer * i}")
+    else:
+        print("This program does not handle negative numbers.")
+    
+
+    run_again= input("Would you like to run the program again? ")
+        
+#While loop ends here
+print("Program is ending! BYE!")
